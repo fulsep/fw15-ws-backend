2fe976e01be5ecf5ddf9bfdc17a7951a77e23086
fulsep <57238803+fulsep@users.noreply.github.com>
2023-08-04 11:39:51 +0700

    fix: allow all cors

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const {Server} = require('socket.io')
 const app = express()
 const server = createServer(app)
 const io = new Server(server, {
-    cors: {
-        origin: process.env.FRONTEND || "http://localhost:5173"
-    }
+    cors: "*"
+    // allowRequest: (_req, cb)=>{
+    //     cb(null, true)
+    // }
 });
 
 io.on("connection", (socket)=>{
